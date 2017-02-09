c575332c774bf60b19e3340a031ab73f39fd8073
mikael.baek <mikael.baek@daumkakao.com>
2017-02-09 16:54:43 +0900

    8.2 Move Inner to upper level
    - move-inner-to-upper-level
    - fix compile errors
    - move down private methods
    - remove dependency to junit, hamcrest

---
+++ src/main/java/pe/msbaek/TyrantMap.java
@@ -0,0 +1,54 @@
+package pe.msbaek;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+class TyrantMap {
+	public static final int OPERATION_PREFIX = 0xC8;
+	public static final int PUT_OPERATION = 0x10;
+	public static final int GET_OPERATION = 0x30;
+	private Socket socket;
+	private DataOutputStream writer;
+	private DataInputStream reader;
+
+	public void put(byte[] key, byte[] value) throws IOException {
+		writeOperation(PUT_OPERATION);
+		writer.writeInt(key.length);
+		writer.writeInt(value.length);
+		writer.write(key);
+		writer.write(value);
+		int status = reader.read();
+		if(status != 0)
+			throw new IllegalStateException();
+	}
+
+	public byte[] get(byte[] key) throws IOException {
+		writeOperation(GET_OPERATION);
+		writer.writeInt(key.length);
+		writer.write(key);
+		int status = reader.read();
+		if(status != 0)
+			throw new IllegalStateException();
+		int size = reader.readInt();
+		byte[] results = new byte[size];
+		reader.read(results);
+		return results;
+	}
+
+	public void open() throws IOException {
+		socket = new Socket("localhost", 1978);
+		writer = new DataOutputStream(socket.getOutputStream());
+		reader = new DataInputStream(socket.getInputStream());
+	}
+
+	public void close() throws IOException {
+		socket.close();
+	}
+
+	private void writeOperation(int putOperation) throws IOException {
+		writer.write(OPERATION_PREFIX);
+		writer.write(putOperation);
+	}
+}
--- src/test/java/pe/msbaek/TyrantMapTest.java
+++ src/test/java/pe/msbaek/TyrantMapTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
-import java.net.Socket;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -40,51 +39,5 @@ public class TyrantMapTest {
 	public void setUp() throws IOException {
 		map = new TyrantMap();
 		map.open();
-	}
-
-	private class TyrantMap {
-		public static final int OPERATION_PREFIX = 0xC8;
-		public static final int PUT_OPERATION = 0x10;
-		public static final int GET_OPERATION = 0x30;
-		private Socket socket;
-		private DataOutputStream writer;
-		private DataInputStream reader;
-
-		public void put(byte[] key, byte[] value) throws IOException {
-			writeOperation(PUT_OPERATION);
-			writer.writeInt(key.length);
-			writer.writeInt(value.length);
-			writer.write(key);
-			writer.write(value);
-			int status = reader.read();
-			assertThat(status, is(0));
-		}
-
-		private void writeOperation(int putOperation) throws IOException {
-			writer.write(OPERATION_PREFIX);
-			writer.write(putOperation);
-		}
-
-		public byte[] get(byte[] key) throws IOException {
-			writeOperation(GET_OPERATION);
-			writer.writeInt(key.length);
-			writer.write(key);
-			int status = reader.read();
-			assertThat(status, is(0));
-			int size = reader.readInt();
-			byte[] results = new byte[size];
-			reader.read(results);
-			return results;
-		}
-
-		private void open() throws IOException {
-			socket = new Socket("localhost", 1978);
-			writer = new DataOutputStream(socket.getOutputStream());
-			reader = new DataInputStream(socket.getInputStream());
-		}
-
-		private void close() throws IOException {
-			socket.close();
-		}
 	}
 }
